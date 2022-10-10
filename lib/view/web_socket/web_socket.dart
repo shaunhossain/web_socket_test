f0803830f6df2b25d5425d9cfc15de80700c7df7
barikoi <shaunhossain95@gmail.com>
2022-10-10 13:27:23 +0600

    add websocket connection and input data from user and show the response on UI

--- lib/view/web_socket/web_socket.dart
+++ lib/view/web_socket/web_socket.dart
@@ -9,35 +9,48 @@ class WebSocketPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetBuilder<WebSocketController>(builder: (controller) {
       return Scaffold(
-          appBar: AppBar(
-            title: const Text('Web Socket'),
-            centerTitle: true,
-          ),
-          body: Obx(() {
-            return controller.messageList == null
-                ? const Center(
-                    child: CircularProgressIndicator(),
-                  )
-                : ListView.builder(
-                    itemCount: controller.messageList.length,
-                    itemBuilder: (context, index) {
-                      return Text(controller.messageList[index]);
-                    });
-          }),
-          bottomNavigationBar: Container(
-            width: MediaQuery.of(context).size.width,
-            height: 60,
-            margin: const EdgeInsets.all(16),
-            child: Card(
-              elevation: 1,
-              color: Colors.blue,
-              child: Row(
-                children: const [
-                  Expanded(child: TextField())
-                ],
-              ),
+        appBar: AppBar(
+          title: const Text('Web Socket'),
+          centerTitle: true,
+        ),
+        body: Obx(() {
+          return controller.messageList == null
+              ? const Center(
+                  child: CircularProgressIndicator(),
+                )
+              : ListView.builder(
+                  itemCount: controller.messageList.length,
+                  reverse: true,
+                  itemBuilder: (context, index) {
+                    return Text(controller.messageList[index]);
+                  });
+        }),
+        bottomSheet: Container(
+          width: MediaQuery.of(context).size.width,
+          height: 60,
+          color: Colors.transparent,
+          margin: const EdgeInsets.all(16),
+          child: Card(
+            elevation: 1,
+            color: Colors.blue,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: TextField(
+                      controller: controller.textEditingController,
+                      decoration:
+                          const InputDecoration(border: OutlineInputBorder())),
+                ),
+                IconButton(
+                    onPressed: () {
+                      controller.insertData();
+                    },
+                    icon: const Icon(Icons.send)),
+              ],
             ),
           ),
+        ),
       );
     });
   }
--- lib/view/web_socket/web_socket_controller/web_socket_controller.dart
+++ lib/view/web_socket/web_socket_controller/web_socket_controller.dart
@@ -1,21 +1,36 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:web_socket_channel/io.dart';
 
 class WebSocketController extends GetxController{
+  final TextEditingController textEditingController = TextEditingController();
 
-  var channel = IOWebSocketChannel.connect(Uri.parse(
+  final _channel = IOWebSocketChannel.connect(Uri.parse(
       'wss://demo.piesocket.com/v3/channel_1?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self'));
   final messageList = [].obs;
 
 
   @override
   void onInit() {
-    channel.stream.listen((message) {
-      messageList.add(message);
-    });
-    Future.delayed(const Duration(seconds: 3),(){
-      channel.sink.add('shaun');
+    _channel.stream.listen((message) {
+      if(message != 'Hello world!'){
+        messageList.add(message);
+      }
     });
     super.onInit();
+  }
+
+  void insertData(){
+    if(textEditingController.text.isNotEmpty){
+      _channel.sink.add(textEditingController.text);
+    }else{
+      _channel.sink.add('empty');
+    }
+  }
+
+  @override
+  void dispose() {
+    _channel.sink.close();
+    super.dispose();
   }
 }
